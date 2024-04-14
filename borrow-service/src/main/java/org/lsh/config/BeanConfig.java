78c49d968e442f117e0dde2ad2840bceff9b06ce
lsh <3040114965@qq.com>
2024-04-14 21:35:17 +0800

    指定负载均衡策略

--- borrow-service/src/main/java/org/lsh/config/BeanConfig.java
+++ borrow-service/src/main/java/org/lsh/config/BeanConfig.java
@@ -1,15 +1,18 @@
 package org.lsh.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(value = "userService", // 指定为userService服务，只要是调用此服务都会使用我们指定的策略
+        configuration = LoanBalancerConfig.class) // 选择我们刚刚配置好的配置类
 public class BeanConfig {
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
+    @LoadBalanced()
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
