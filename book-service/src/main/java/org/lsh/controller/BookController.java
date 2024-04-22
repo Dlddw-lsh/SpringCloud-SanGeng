70694265eeb6fb8edd4624cba57f99812de9e7d5
lsh <3040114965@qq.com>
2024-04-22 16:34:59 +0800

    配置文件热部署

--- book-service/src/main/java/org/lsh/controller/BookController.java
+++ book-service/src/main/java/org/lsh/controller/BookController.java
@@ -3,13 +3,19 @@ package org.lsh.controller;
 import com.lsh.entity.Book;
 import jakarta.annotation.Resource;
 import org.lsh.service.BookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RefreshScope // 实现Nacos配置中心实时刷新
 public class BookController {
+
+    @Value("${test.hello}")
+    String hello;
 
     @Resource
     BookService service;
@@ -18,5 +24,10 @@ public class BookController {
     Book findBookById(@PathVariable("bid") int bid){
         System.out.println("bookService");
         return service.getBookById(bid);
+    }
+
+    @RequestMapping("/test")
+    String test(){
+        return hello;
     }
 }
