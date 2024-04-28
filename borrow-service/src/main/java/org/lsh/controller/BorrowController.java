318197b1e94da9c21f3424a7133f3ece94bc426c
lsh <3040114965@qq.com>
2024-04-28 22:01:13 +0800

    限流的异常处理

--- borrow-service/src/main/java/org/lsh/controller/BorrowController.java
+++ borrow-service/src/main/java/org/lsh/controller/BorrowController.java
@@ -1,5 +1,7 @@
 package org.lsh.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.lsh.entity.Result;
 import jakarta.annotation.Resource;
 import org.lsh.entity.UserBorrowDetail;
@@ -7,6 +9,8 @@ import org.lsh.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 
 @RestController
@@ -21,7 +25,27 @@ public class BorrowController {
     }
 
     @RequestMapping("/blocked")
-    public Result<String> blocked(){
+    public Result<String> blocked() {
         return Result.ok("请求频率过快");
+    }
+
+    @RequestMapping("/test")
+    @SentinelResource(
+            value = "test",
+            fallback = "except",
+            blockHandler = "block",
+            exceptionsToIgnore = IOException.class)
+    public String test() {
+        throw new RuntimeException("Hello World");
+    }
+
+    // 替代方法名必须和原方法参数一致，最后可以添加一个Throwable作为参数接收异常
+    String except(Throwable t) {
+        return t.getMessage();
+    }
+
+    // 如果blockHandler和fallback都指定了，出现限流的异常就走block，其他异常走fallback
+    String block(BlockException e) {
+        return "被限流了";
     }
 }
